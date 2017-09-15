9496a70b1c1e0a1b2c05aea4d8704ea1dedbdab9
Hileo Andersson <hileomsi@gmail.com>
2017-09-15 18:13:16 -0300

    curry function, version 1.0.5

--- src/index.js
+++ src/index.js
@@ -8,24 +8,31 @@ const getDefaultClassName = (defaultClassName, { className }) => {
   return `${defaultClassName.trim()} ${className ? className.trim() : ''}`;
 }
 
-const createClassName = (defaultClassName = '', props = {}, mapping = []) => {
-  const getClassName = (className) => {
-    return (typeof className === 'function') ? className(props) : className;
+const createClassName = (defaultClassName = '', mapping = [], removeProp = true) => {
+  
+  return ( props = {} ) => {
+
+    const getClassName = (verifier, className) => {
+      if(removeProp) delete props[verifier];
+      return (typeof className === 'function') ? className(props) : className;
+    }
+  
+    if(typeof defaultClassName != 'string' || typeof props != 'object' || !Array.isArray(mapping))
+      throw new Error('TypeError: invalid arguments types');
+  
+    defaultClassName = getDefaultClassName(defaultClassName, props);
+    return mapping.reduce((previous, current, index) => {
+      if(typeof current.verifier === 'string' && props[current.verifier]){
+        return `${previous} ${getClassName(current.verifier, current.className)}`;
+      } else if(typeof current.verifier === 'function' && current.verifier(props) === true) {
+        return `${previous} ${getClassName(current.verifier(props), current.className)}`;
+      }
+  
+      return previous;
+    }, defaultClassName);
+
   }
 
-  if(typeof defaultClassName != 'string' || typeof props != 'object' || !Array.isArray(mapping))
-    throw new Error('TypeError: invalid arguments types');
-
-  defaultClassName = getDefaultClassName(defaultClassName, props);
-  return mapping.reduce((previous, current, index) => {
-    if(typeof current.verifier === 'string' && props[current.verifier]){
-      return `${previous} ${getClassName(current.className)}`;
-    } else if(typeof current.verifier === 'function' && current.verifier(props) === true) {
-      return `${previous} ${getClassName(current.className)}`;
-    }
-
-    return previous;
-  }, defaultClassName);
 }
 
 module.exports = {
